bdf4122aa776a4c36b052f3cab42786abdd18640
Sergii [boonya] Buinytskyi <boonya41@gmail.com>
2017-08-08 18:12:27 +0300

    Yii via composer

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii-1.1.19.5790cb/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+require_once 'vendor/autoload.php';
+$config = dirname(__FILE__) . '/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 // specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
-require_once($yii);
 Yii::createWebApplication($config)->run();
