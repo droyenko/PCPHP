697761a35004f94bdfff458574f16bc42356d3bf
Vasya <Vasyabest@rambler.ru>
2017-08-14 08:03:25 +0300

    replaced everywhere GET on not-global

--- protected/controllers/GroupController.php
+++ protected/controllers/GroupController.php
@@ -5,9 +5,10 @@ class GroupController extends AbstactController
     public function actionCreateGroup()
     {
         $newGroup = new GroupForm();
+        $createGroupPost = Yii::app()->request->getPost('CreateForm');
 
-        if (isset($_POST['CreateForm'])) {
-            $newGroup->attributes = Yii::app()->request->getPost('CreateForm');
+        if (isset($createGroupPost)) {
+            $newGroup->attributes = $createGroupPost;
 
             if (!$newGroup->validate()) {
                 throw new CHttpException(404,'error in request');
@@ -34,8 +35,10 @@ class GroupController extends AbstactController
 
     public function actionDeleteGroup()
     {
+        $id = Yii::app()->request->getParam('id');
+
         Yii::app()->db->createCommand()
-            ->delete('db_groups', 'id=:id', [':id'=>Yii::app()->request->getParam('id')])
+            ->delete('db_groups', 'id=:id', [':id'=>$id])
             ->execute();
 
         echo true;
