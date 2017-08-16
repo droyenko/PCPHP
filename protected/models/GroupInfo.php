7361ef3c673c2c9ed5d3555d199b2dd264ddb6f9
Dmytro Roienko <dmytro.royenko@gmail.com>
2017-08-16 14:35:05 +0300

    desperadly trying to understand git

---
+++ protected/models/GroupInfo.php
@@ -0,0 +1,23 @@
+<?php
+
+class Notifications extends CActiveRecord
+{
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    public function tableName()
+    {
+        return 'locations';
+    }
+
+    public function getLocations()
+    {
+        $criteria = new CDbCriteria();
+        $criteria->select = 'name';
+        $locations = Locations::model()->findAll($criteria);
+
+        return $locations;
+    }
+}
