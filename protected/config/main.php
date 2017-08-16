fced13b510170ae0212f6a7bbd38c68feb3b2110
Vasya <Vasyabest@rambler.ru>
2017-08-16 23:31:28 +0300

    refactored config main

--- protected/config/main.php
+++ protected/config/main.php
@@ -31,31 +31,24 @@ return [
         */
     ],
 
+
     // application components
     'components' => [
-                'urlManager' => [
-                       'showScriptName' => false,
-                        'urlFormat' => 'path',
-                        'rules' => [
-                                '<controller:\w+>/<id:\d+>' => '<controller>/view',
-                                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
-                                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-                            ],
-                    ],
+        'urlManager' => [
+            'showScriptName' => false,
+            'urlFormat' => 'path',
+            'rules' => [
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ],
+        ],
+        'user' => [
+            // enable cookie-based authentication
+            'allowAutoLogin' => true,
+        ],
 
-        // uncomment the following to enable URLs in path-format
-        /*
-        'urlManager'=>array(
-            'urlFormat'=>'path',
-            'rules'=>array(
-                '<controller:\w+>/<id:\d+>'=>'<controller>/view',
-                '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-            ),
-        ),
-        */
-
-        // database settings are configured in database.php
+            // database settings are configured in database.php
         'db' => require(dirname(__FILE__) . '/database.php'),
 
         'errorHandler' => [
