ed37e293ebad103758582a6a5a09a171ea2f75ab
voldan <vladdan1980@gmail.com>
2017-09-05 15:22:55 +0300

    studentListEdit-score Modal controller

--- protected/controllers/StudentListController.php
+++ protected/controllers/StudentListController.php
@@ -1,10 +1,32 @@
 <?php
 
-class StudentListController extends BaseController
+class StudentListController extends CController
 {
-    public function actionGetStudentList()
+    public function actionRenderEditModalScore($group_id)
     {
-        $students = StudentList::model()->getStudentList();
-        $this->renderJSON($students);
+        /** @var StudentComponent $component */
+        $component = Yii::app()->getComponent('Student');
+        $rawData = $component->getStudentList($group_id);
+
+
+        $studentList = [];
+        foreach ($rawData as $student) {
+            $studentList[] = [
+                'id' => $student['id'],
+                'name' => $student['first_name'],
+                'surname' => $student['last_name'],
+                'photo_url' => $student['photo_url'],
+                'incoming_test' => $student['incoming_test'],
+                'entry_score' => $student['entry_score'],
+                'approved_by' => $student['approved_by'],
+            ];
+        }
+
+        $dataArray = [
+            'groupName' => Yii::app()->user->groupName,
+            'groupDirection' => Yii::app()->user->groupDirection,
+            'studentList' => $studentList
+            ];
+        $this->renderPartial('studentScore', $dataArray);
     }
 }
