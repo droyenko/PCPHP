31caa8336507ab497cbca3f06054613c04300571
voldan <vladdan1980@gmail.com>
2017-09-05 22:55:31 +0300

    requested changes

--- protected/commands/DemoDataCommand.php
+++ protected/commands/DemoDataCommand.php
@@ -2,95 +2,70 @@
 
 class DemoDataCommand extends CConsoleCommand
 {
-    // $app = Yii::app();
-    // const USERS = Yii::app()->basePath . '/data/demo/users.php';
-    // const LOCATIONS = Yii::app()->basePath . '/data/demo/locations.php';
-    // const GROUPS = Yii::app()->basePath . '/data/demo/groups.php';
-    // const USER_GROUPS = Yii::app()->basePath . '/data/demo/user_groups.php';
-    // const USER_ROLES = Yii::app()->basePath . '/data/demo/user_roles.php';
-    // const DIRECTIONS = Yii::app()->basePath . '/data/demo/directions.php';
-
     public function actionFillOutAllTables()
     {
-        $this->actionFillOutTableLocations();
-        $this->actionFillOutTableUsers();
-        $this->actionFillOutTableDirections();
-        $this->actionFillOutTableGroups();
-        $this->actionFillOutTableUserGroups();
-        $this->actionFillOutTableUserRoles();
-        $this->actionFillOutTableExperts();
-        $this->actionFillOutTableStudents();
+        $this->actionFillOutTableLocations('locations');
+        $this->actionFillOutTableUsers('users');
+        $this->actionFillOutTableDirections('directions');
+        $this->actionFillOutTableGroups('groups');
+        $this->actionFillOutTableUserGroups('user_groups');
+        $this->actionFillOutTableUserRoles('user_roles');
+        $this->actionFillOutTableExperts('experts');
+        $this->actionFillOutTableStudents('students');
     }
 
-    public function actionFillOutTableLocations()
+    private function getDemoData($fileName)
     {
-        $locations = require_once Yii::app()->basePath . '/data/demo/locations.php';
+        return $demoData = require_once Yii::app()->basePath . '/data/demo/' . $fileName;
+    }
+
+    private function fillOutTable($tableName)
+    {
+        $fileName = $tableName . '.php';
+        $demoData = $this->getDemoData($fileName);
         $command = Yii::app()->db->createCommand();
-        foreach ($locations as $location) {
-            $command->insert('locations', $location);
+        foreach ($demoData as $columnsArr) {
+            $command->insert($tableName, $columnsArr);
         }
     }
 
-    public function actionFillOutTableUsers()
+    public function actionFillOutTableLocations($tableName)
     {
-        $users = require_once Yii::app()->basePath . '/data/demo/users.php';
-        $command = Yii::app()->db->createCommand();
-        foreach ($users as $user) {
-            $command->insert('users', $user);
-        }
+        $this->fillOutTable($tableName);
     }
 
-    public function actionFillOutTableDirections()
+    public function actionFillOutTableUsers($tableName)
     {
-        $directions = require_once Yii::app()->basePath . '/data/demo/directions.php';
-        $command = Yii::app()->db->createCommand();
-        foreach ($directions as $direction) {
-            $command->insert('directions', $direction);
-        }
+        $this->fillOutTable($tableName);
     }
 
-    public function actionFillOutTableGroups()
+    public function actionFillOutTableDirections($tableName)
     {
-        $groups = require_once Yii::app()->basePath . '/data/demo/groups.php';
-        $command = Yii::app()->db->createCommand();
-        foreach ($groups as $group) {
-            $command->insert('groups', $group);
-        }
+        $this->fillOutTable($tableName);
     }
 
-    public function actionFillOutTableUserGroups()
+    public function actionFillOutTableGroups($tableName)
     {
-        $user_groups = require_once Yii::app()->basePath . '/data/demo/user_groups.php';
-        $command = Yii::app()->db->createCommand();
-        foreach ($user_groups as $user_group) {
-            $command->insert('user_groups', $user_group);
-        }
+        $this->fillOutTable($tableName);
     }
 
-    public function actionFillOutTableUserRoles()
+    public function actionFillOutTableUserGroups($tableName)
     {
-        $user_roles = require_once Yii::app()->basePath . '/data/demo/user_roles.php';
-        $command = Yii::app()->db->createCommand();
-        foreach ($user_roles as $user_role) {
-            $command->insert('user_roles', $user_role);
-        }
+        $this->fillOutTable($tableName);
     }
 
-    public function actionFillOutTableExperts()
+    public function actionFillOutTableUserRoles($tableName)
     {
-        $demodata = require_once Yii::app()->basePath . '/data/demo/experts.php';
-        $command = Yii::app()->db->createCommand();
-        foreach ($demodata as $columnsArr) {
-            $command->insert('experts', $columnsArr);
-        }
+        $this->fillOutTable($tableName);
     }
 
-    public function actionFillOutTableStudents()
+    public function actionFillOutTableExperts($tableName)
     {
-        $demodata = require_once Yii::app()->basePath . '/data/demo/students.php';
-        $command = Yii::app()->db->createCommand();
-        foreach ($demodata as $columnsArr) {
-            $command->insert('students', $columnsArr);
-        }
+        $this->fillOutTable($tableName);
+    }
+
+    public function actionFillOutTableStudents($tableName)
+    {
+        $this->fillOutTable($tableName);
     }
 }
